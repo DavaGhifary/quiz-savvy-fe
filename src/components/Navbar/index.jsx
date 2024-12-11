a312cb63fb6244849d352f97042921adac5d9e40
Dava Ghifary <davaghifry@gmail.com>
2024-12-11 22:29:02 +0700

    refactor:edit home page

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,12 +1,14 @@
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Logo from "../../assets/img/Logo.png";
 
 const Navbar = () => {
   return (
-    <div className="w-full">
-      <div className="flex justify-between items-center h-16 mx-10">
-        <div>
-          <p>Navbar</p>
+    <div className="w-full fixed bg-white z-10">
+      <div className="flex justify-between items-center h-16 mx-[4rem]">
+        <div className="flex items-center">
+          <img src={Logo} width={60} />
+          <p className="font-semibold text-lg">Savvy</p>
         </div>
         <div className="flex items-center gap-20">
           <div className="relative">
@@ -16,7 +18,10 @@ const Navbar = () => {
               className="px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 w-60 rounded-full sm:text-sm focus:ring-1"
               placeholder="Search Quiz"
             />
-            <FontAwesomeIcon icon={faMagnifyingGlass} className="absolute right-3 top-2 text-slate-300" />
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className="absolute right-3 top-2 text-slate-300"
+            />
           </div>
           <div>
             <button
